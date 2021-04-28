929ad50e269b6bef042fb172b8f54f3118f98c12
Botspot <j.allen.marchand@outlook.com>
2021-04-28 13:21:28 -0500

    Make 'none' screensaver work

--- none/install.sh
+++ none/install.sh
@@ -1,13 +1,1 @@
 #!/bin/bash
-
-echo "Creating systemd service for '$currentclick'..."
-#Stop existing screensaver.service
-if [ -f /etc/systemd/system/screensaver.service ]; then
-  echo "Stopping the service first..."
-  sudo systemctl stop screensaver	
-  sudo systemctl disable screensaver
-  sudo rm /etc/systemd/system/screensaver.service
-fi
-
-echo "Reloading systemd daemon..."
-sudo systemctl daemon-reload
