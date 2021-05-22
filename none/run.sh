8fbea8e9a87986d29d7afca10d8a6127624e719c
Botspot <j.allen.marchand@outlook.com>
2021-05-22 15:14:30 -0500

    make script for 'none' screensaver

--- none/run.sh
+++ none/run.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+if [ -f /etc/systemd/system/screensaver.service ]; then
+  echo "Stopping systemd service..."
+  sudo systemctl stop screensaver
+  
+  echo "Disabling service..."
+  sudo systemctl disable screensaver
+  
+  echo "Removing service..."
+  sudo rm -f /etc/systemd/system/screensaver.service
+fi
+
+echo "Removing daemon script..."
+rm -f ~/.screensavers/daemon.sh
