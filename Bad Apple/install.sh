512f061de5a510850c18d231b782838f9feed426
Botspot <54716352+Botspot@users.noreply.github.com>
2024-03-01 00:23:56 -0600

    Add Bad Apple screensaver

---
+++ Bad Apple/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#On my system this is translated to ~/Screensavers/<screensaver name>
+FILES="$(dirname "$0")"
+export WINEPREFIX="$HOME/.screensavers"
+
+#Setup wine prefix
+if [ ! -e "$HOME/.screensavers" ]; then
+	wine wineboot || exit 1
+	wineserver -w
+fi
+
+#Install screensaver
+cp "$FILES/badapple.scr" "$WINEPREFIX/drive_c/windows/"
